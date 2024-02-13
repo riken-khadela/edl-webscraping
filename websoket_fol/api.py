9046f686cc3d0b33773dd8b120426b6dcb1f4cfb
riken-khadela <rikenkhadelamain@gmail.com>
2024-02-13 20:23:13 +0530

    added to run continously

--- websoket_fol/api.py
+++ websoket_fol/api.py
@@ -10,21 +10,15 @@ bot_ = Bot()
 bot_.get_local_driver()
 bot_.work()
 
+
 async def echo(websocket, path):
     connected.add(websocket)
-    try:
-        async for message in websocket:
+    while True:
+        if connected:
+            main_data = await bot_.return_main_data_for_all_windows_parallel()
             for conn in connected:
-                await conn.send(json.dumps({"data": "websocket has been connected !"}))
-                await websocket.send(f"Echo: {message}")
-                try:
-                    main_data = await bot_.return_main_data_for_all_windows_parallel()
-                except Exception as e : websocket.send(f"error: {message}")
                 await conn.send(json.dumps({"data": main_data}))
-                print(f'\n\n\n Dattime : {datetime.datetime.now()}')
-            await websocket.send(f"Echo: {message}")
-    finally:
-        connected.remove(websocket)
+    connected.remove(websocket)
 
 start_server = websockets.serve(echo, "0.0.0.0", 8765)
 
